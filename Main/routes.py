a1fff72dfc60b99e5fff43ca378dd851c0f21e8b
Shivaraj Shetty <shivarajshetty635@gmail.com>
2024-11-25 12:55:56 +0530

    Added login feature and styling

--- Main/routes.py
+++ Main/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from Main.models import insert_user, get_all_users
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from Main.models import insert_user, get_all_users, User
 
 app_routes = Blueprint('app_routes', __name__)
+
 
 @app_routes.route('/')
 def index():
@@ -24,3 +25,23 @@ def signup():
 def view_db():
     users = get_all_users()
     return render_template('success.html', users=users)
+
+@app_routes.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Fetch user from the database
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:  # Compare passwords
+            # Valid credentials
+            users = get_all_users()
+            return render_template('success.html', users = users)
+        else:
+            # Invalid credentials
+            flash('Invalid username or password', 'error')
+            return render_template('login.html')  # Re-render login page with error
+
+    return render_template('login.html')  # GET request renders login page
